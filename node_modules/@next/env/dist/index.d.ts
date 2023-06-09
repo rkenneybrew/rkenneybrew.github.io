a42eb2de36420d98ec137b883c0085bd2dc504f3
Rickey Kenneybrew <rkenneybrew88@gmail.com>
2023-06-08 21:40:28 -0700

    update

---
+++ node_modules/@next/env/dist/index.d.ts
@@ -0,0 +1,19 @@
+export declare type Env = {
+    [key: string]: string | undefined;
+};
+export declare type LoadedEnvFiles = Array<{
+    path: string;
+    contents: string;
+}>;
+export declare let initialEnv: Env | undefined;
+export declare function updateInitialEnv(newEnv: Env): void;
+declare type Log = {
+    info: (...args: any[]) => void;
+    error: (...args: any[]) => void;
+};
+export declare function processEnv(loadedEnvFiles: LoadedEnvFiles, dir?: string, log?: Log, forceReload?: boolean): Env;
+export declare function loadEnvConfig(dir: string, dev?: boolean, log?: Log, forceReload?: boolean): {
+    combinedEnv: Env;
+    loadedEnvFiles: LoadedEnvFiles;
+};
+export {};
