e31380ba9079cf4a3c110a52acbfe6a951613599
Rickey Kenneybrew <rkenneybrew88@gmail.com>
2023-12-23 22:24:58 -0800

    update - Rollup.js
    Infrastructure.

--- src/js/Rollup/_config/ctm/custom.rollup.config.mjs
+++ src/js/Rollup/_config/ctm/custom.rollup.config.mjs
@@ -1,7 +1,7 @@
 export default {
-    input: '/Users/rkenneybrew88/src/rollup/umd/main.js',
+    input: '/umd/main.js',
     output: {
-      file: '/Users/rkenneybrew88/src/rollup/umd/build/name_this_prefix.min.js',
+      file: '/umd/build/name_this_prefix.min.js',
       format: 'cjs',
       sourcemap: 'inline',
     },
